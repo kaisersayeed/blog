fa06f78618dd65213f2e8b3283bfed1f98284bd9
KAISER SAYEED <kaiser.sayeed07@gmail.com>
2018-01-14 00:21:29 +0600

    Set up initial project skeleton

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <div>
+    This is a simple blog App.
+  </div>
+);
+
+ReactDOM.render(
+  <App />,
+  document.querySelector(".container")
+);
