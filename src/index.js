deefd5ac35af517659dce4c1d3f2a10284e7f926
KAISER SAYEED <kaiser.sayeed07@gmail.com>
2018-01-14 00:51:00 +0600

    Fetched posts

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
+import reducers from "./reducers";
 
-const App = () => (
-  <div>
-    This is a simple blog App.
-  </div>
-);
+import PostsIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+        <br/>
+        <br/>
+        <br/>
+      </div>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector(".container")
 );
